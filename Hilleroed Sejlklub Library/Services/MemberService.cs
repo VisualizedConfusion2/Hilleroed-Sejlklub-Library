9af539db52904e88021e3528372448a3968d5125
VisualizedConfusion2 <thh005@edu.zealand.dk>
2025-04-28 21:47:51 +0200

    services

--- Hilleroed Sejlklub Library/Services/MemberService.cs
+++ Hilleroed Sejlklub Library/Services/MemberService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hilleroed_Sejlklub_Library.Interfaces;
+using Hilleroed_Sejlklub_Library.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,76 @@ using System.Threading.Tasks;
 
 namespace Hilleroed_Sejlklub_Library.Services
 {
-    internal class MemberService
+    internal class MemberService : IMemberService
     {
+        private readonly List<Member> _members;
+
+        public MemberService()
+        {
+            _members = new List<Member>();
+        }
+
+        public IEnumerable<Member> GetAllMembers()
+        {
+            return _members;
+        }
+
+        public Member? GetMemberById(int id)
+        {
+            return _members.FirstOrDefault(m => m.MemberID == id);
+        }
+
+        public void AddMember(Member member)
+        {
+            _members.Add(member);
+        }
+
+        public bool UpdateMember(int id, Member updatedMember)
+        {
+            var member = GetMemberById(id);
+            if (member == null)
+                return false;
+
+            member.Name = updatedMember.Name;
+            member.ContactInfo = updatedMember.ContactInfo;
+            member.Birthday = updatedMember.Birthday;
+            member.Gender = updatedMember.Gender;
+            return true;
+        }
+
+        public bool DeleteMember(int id)
+        {
+            var member = GetMemberById(id);
+            if (member == null)
+                return false;
+
+            _members.Remove(member);
+            return true;
+        }
+
+        public Member GetMember(int MemberID)
+        {
+            return GetMemberById(MemberID) ?? throw new KeyNotFoundException($"Member with ID {MemberID} not found.");
+        }
+
+        public List<Member> GetMembers()
+        {
+            return _members.ToList();
+        }
+
+        public void Add(Member member)
+        {
+            if (_members.Any(m => m.MemberID == member.MemberID))
+                throw new InvalidOperationException($"Member with ID {member.MemberID} already exists.");
+            _members.Add(member);
+        }
+
+        public void Delete(int MemberID)
+        {
+            var member = GetMemberById(MemberID);
+            if (member == null)
+                throw new KeyNotFoundException($"Member with ID {MemberID} not found.");
+            _members.Remove(member);
+        }
     }
 }
--- Hilleroed Sejlklub Library/Services/RepairService.cs
+++ Hilleroed Sejlklub Library/Services/RepairService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hilleroed_Sejlklub_Library.Interfaces;
+using Hilleroed_Sejlklub_Library.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,84 @@ using System.Threading.Tasks;
 
 namespace Hilleroed_Sejlklub_Library.Services
 {
-    internal class RepairService
+    internal class RepairService : IRepair
     {
+        private readonly List<Repair> _repairs;
+
+        public RepairService()
+        {
+            _repairs = new List<Repair>();
+        }
+
+        public IEnumerable<Repair> GetAllRepairs()
+        {
+            return _repairs;
+        }
+
+        public Repair? GetRepairById(string id) // Changed parameter type from int to string
+        {
+            return _repairs.FirstOrDefault(r => r.RepairID == id); // No changes needed here as both are strings now
+        }
+
+        public void AddRepair(Repair repair)
+        {
+            if (_repairs.Any(r => r.RepairID == repair.RepairID))
+                throw new InvalidOperationException($"Repair with ID {repair.RepairID} already exists.");
+            _repairs.Add(repair);
+        }
+
+        public bool UpdateRepair(string id, Repair updatedRepair) // Changed parameter type from int to string
+        {
+            var repair = GetRepairById(id);
+            if (repair == null)
+                return false;
+
+            repair.Description = updatedRepair.Description;
+            repair.Date = updatedRepair.Date;
+            return true;
+        }
+
+        public bool DeleteRepair(string id) // Changed parameter type from int to string
+        {
+            var repair = GetRepairById(id);
+            if (repair == null)
+                return false;
+
+            _repairs.Remove(repair);
+            return true;
+        }
+
+        public Repair GetRepair(string repairID) // Changed parameter type from int to string
+        {
+            return GetRepairById(repairID) ?? throw new KeyNotFoundException($"Repair with ID {repairID} not found.");
+        }
+
+        public List<Repair> GetRepairs()
+        {
+            return _repairs.ToList();
+        }
+
+        public void Delete(string repairID) // Changed parameter type from int to string
+        {
+            var repair = GetRepairById(repairID);
+            if (repair == null)
+                throw new KeyNotFoundException($"Repair with ID {repairID} not found.");
+            _repairs.Remove(repair);
+        }
+
+        public List<Repair> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Repair repair)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int RepairID)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
