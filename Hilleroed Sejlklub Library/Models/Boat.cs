f2fe94cd55216d158d28038f69b0a09e7a3a6503
Egil Bukhave Edström <ege001@edu.zealand.dk>
2025-04-28 12:27:07 +0200

    Member class properties / constructor

--- Hilleroed Sejlklub Library/Models/Boat.cs
+++ Hilleroed Sejlklub Library/Models/Boat.cs
@@ -2,5 +2,22 @@
 {
     public class Boat
     {
+        /// <summary>
+        /// En Member class repræsenterer et medlem af Hillerød sejlklub
+        /// </summary>
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string ContactInfo { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Gender { get; set; }
+
+        public Boat(string name, int id, string contactInfo, DateTime birthday, string gender)
+        {
+            Name = name;
+            Id = id;
+            ContactInfo = contactInfo;
+            Birthday = birthday;
+            Gender = gender;
+        }
     }
 }
