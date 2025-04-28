e12fa2dd8128fba2339b2b454d7cae50c6a641d0
VisualizedConfusion2 <thh005.edu.zealand.dk>
2025-04-28 13:20:17 +0200

    irepair

--- Hilleroed Sejlklub Library/Interfaces/IRepair.cs
+++ Hilleroed Sejlklub Library/Interfaces/IRepair.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hilleroed_Sejlklub_Library.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace Hilleroed_Sejlklub_Library.Interfaces
 {
     interface IRepair
     {
+        // Get all repair records
+        List<Repair> GetAll();
+
+        // Add a new repair record
+        void Add(Repair repair);
+
+        // Delete a repair record by ID
+        void Delete(int Id);
     }
 }
