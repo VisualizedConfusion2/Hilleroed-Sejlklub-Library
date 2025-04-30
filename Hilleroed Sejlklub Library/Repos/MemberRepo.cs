7986db2151beb39c1e0cde6c4d21b5237a2a46ed
VisualizedConfusion2 <thh005@edu.zealand.dk>
2025-04-30 10:16:32 +0200

    opretter member repo

--- Hilleroed Sejlklub Library/Repos/MemberRepo.cs
+++ Hilleroed Sejlklub Library/Repos/MemberRepo.cs
@@ -1,12 +1,89 @@
-﻿using System;
+﻿using Hilleroed_Sejlklub_Library.Interfaces;
+using Hilleroed_Sejlklub_Library.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hilleroed_Sejlklub_Library.Repos
 {
-    internal class MemberRepo
+    public class MemberRepo : IMemberRepo
     {
+        private List<Member> _members;
+
+        public MemberRepo()
+        {
+            _members = new List<Member>();
+        }
+
+        public void Add(Member member)
+        {
+            if (member == null)
+            {
+                throw new ArgumentNullException("member");
+            }
+
+            _members.Add(member);
+        }
+
+        public void Delete(int MemberID)
+        {
+            Member memberToDelete = null;
+
+            foreach (Member member in _members)
+            {
+                if (member.MemberID == MemberID)
+                {
+                    memberToDelete = member;
+                    break;
+                }
+            }
+
+            if (memberToDelete != null)
+            {
+                _members.Remove(memberToDelete);
+            }
+        }
+
+        public List<Member> Get()
+        {
+            return _members;
+        }
+
+        public Member GetMember(int MemberID)
+        {
+            foreach (Member member in _members)
+            {
+                if (member.MemberID == MemberID)
+                {
+                    return member;
+                }
+            }
+
+            return null;
+        }
+
+        public string GetContactInfo(int MemberID)
+        {
+            foreach (Member member in _members)
+            {
+                if (member.MemberID == MemberID)
+                {
+                    return $"Email: {member.ContactInfo}";
+                }
+            }
+
+            return "Kontaktinformation ikke fundet.";
+        }
+
+        public string GetGender(int MemberID)
+        {
+            foreach (Member member in _members)
+            {
+                if (member.MemberID == MemberID)
+                {
+                    return member.Gender;
+                }
+            }
+            return "Ukendt køn";
+        }
     }
 }
